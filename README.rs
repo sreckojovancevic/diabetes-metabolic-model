3d1114ca520792f30ea48ba5fcee5c275f74bc13
Srecko Jovancevic <srecko.jovancevic@gmail.com>
2026-07-27 22:52:47 +0200

    Create README.rs

---
+++ README.rs
@@ -0,0 +1,158 @@
+# Model metabolizma kod dijabetesa
+
+> **Edukativni model sistemskog prikaza fizioloških procesa kod dijabetesa, zasnovan na ličnom iskustvu autora i javno dostupnim fiziološkim saznanjima.**
+
+---
+
+## Svrha projekta
+
+Cilj ovog projekta je da na jednostavan i razumljiv način prikaže međusobnu povezanost fizioloških procesa koji učestvuju u regulaciji glukoze kod osoba sa dijabetesom.
+
+Model ne posmatra dijabetes isključivo kao poremećaj nivoa šećera u krvi, već kao složen sistem međusobno povezanih procesa koji uključuju:
+
+* medicinski propisanu ishranu;
+* regulaciju glukoze u krvi;
+* terapiju insulinom;
+* oralnu antidijabetičku terapiju;
+* funkciju jetre;
+* odgovor organizma na stres;
+* anksioznost;
+* zaštitu kardiovaskularnog sistema.
+
+Ovaj projekat ima isključivo edukativnu svrhu.
+
+---
+
+## Pregled modela
+
+```text
+                     +---------------------------------------+
+                     |      MEDICINSKI PROPISANA ISHRANA     |
+                     | (Lekar / Nutricionista)               |
+                     +---------------------------------------+
+                                         |
+                                         v
+                             [ Nivo glukoze u krvi ]
+                                         |
+                    +--------------------+--------------------+
+                    |                                         |
+                    v                                         v
+           +-----------------+                     +----------------------+
+           |    INSULIN      |                     | ORALNI ANTIDIJABETIK |
+           +-----------------+                     +----------------------+
+                    |                                         |
+                    |                                 Smanjuje stvaranje
+                    |                                 glukoze u jetri
+                    |                                         |
+                    +--------------------+--------------------+
+                                         |
+                                         v
+                               +----------------------+
+                               |        JETRA         |
+                               +----------------------+
+                               | Regulacija glukoze   |
+                               | Glukoneogeneza       |
+                               | Metabolizam lekova   |
+                               +----------------------+
+                                         |
+                    +--------------------+--------------------+
+                    |                                         |
+                    v                                         v
+            +----------------+                      +----------------------+
+            |  ANKSIOZNOST   |                      | ANTIAGREGACIONA      |
+            +----------------+                      | TERAPIJA             |
+                    |                              +----------------------+
+                    v
+          Kortizol i adrenalin
+                    |
+                    v
+      +----------------------------------+
+      | Povećana proizvodnja glukoze     |
+      | Smanjena efikasnost insulina     |
+      +----------------------------------+
+                    |
+                    v
+          Povećana glikemija
+                    |
+                    v
+          +----------------------+
+          | Anksiolitik / Sedativ|
+          +----------------------+
+                    |
+                    v
+      +--------------------------------------+
+      | KARDIOVASKULARNI SISTEM              |
+      |                                      |
+      | Beta blokator                        |
+      | • Regulacija krvnog pritiska         |
+      | • Regulacija srčane frekvencije      |
+      | • Može prikriti simptome             |
+      |   hipoglikemije                      |
+      +--------------------------------------+
+```
+
+---
+
+## Objašnjenje modela
+
+Model prikazuje međusobnu povezanost između:
+
+* ishrane;
+* regulacije glukoze u krvi;
+* terapije insulinom;
+* oralne antidijabetičke terapije;
+* funkcije jetre;
+* hormona stresa;
+* psiholoških faktora;
+* zaštite kardiovaskularnog sistema.
+
+Njegova svrha je da složene fiziološke procese predstavi kroz jedinstven sistemski prikaz koji je lakši za razumevanje.
+
+---
+
+## Napomena
+
+Ovaj projekat **ne predstavlja medicinski vodič** i ne daje preporuke za dijagnostiku ili lečenje.
+
+Svaka terapija mora biti određena od strane endokrinologa ili drugog kvalifikovanog zdravstvenog radnika.
+
+---
+
+## Lična napomena autora
+
+Inspiracija za ovaj projekat proistekla je iz mog ličnog iskustva života sa dijabetesom.
+
+Tokom godina lečenja postalo mi je jasno da uspešna regulacija glikemije ne zavisi samo od insulina i ishrane, već i od funkcije jetre, hormona stresa, psihološkog stanja i zdravlja kardiovaskularnog sistema.
+
+Razgovori sa lekarima, sopstveno iskustvo i proučavanje dostupne stručne literature podstakli su me da napravim ovaj edukativni model kako bi i drugi lakše razumeli međusobnu povezanost ovih procesa.
+
+---
+
+## Doprinos projektu
+
+Predlozi, ispravke i konstruktivne sugestije su dobrodošli.
+
+Posebno su dobrodošli doprinosi:
+
+* endokrinologa;
+* lekara;
+* nutricionista;
+* psihologa;
+* istraživača;
+* osoba koje žive sa dijabetesom.
+
+---
+
+## Licenca
+
+Ovaj projekat je licenciran pod **Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)** licencom.
+
+Detalji se nalaze u fajlu **LICENSE**.
+
+---
+
+## Autor
+
+**Srećko Jovančević**
+
+GitHub: https://github.com/sreckojovancevic
